013b2da13013139426e4480a8f0e957d291ba3a5
qdung030404 <qdung030404@gmail.com>
2026-06-03 12:33:59 +0700

    CombineTripsManagement:   + Rename from BookingManagement to align with the "Combine Trips" feature.   + Integrate the new CombineTripsCard for a request list. - Implement CombineTripsCard:   + Design a detailed card UI displaying passenger info, formatted phone numbers, total fare, and payment methods.   + Integrate BuildWidget for a consistent location/route visualization.   + Add trip statistics: Route distance (km) and estimated duration.   + Add action buttons for drivers to "Accept" or "Reject" incoming passenger requests.

---
+++ lib/views/driver/booking_management/widget/combine_trips_card.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:optigo/models/booking_model.dart';
+import 'package:optigo/providers/booking_provider.dart';
+import 'package:optigo/providers/map_provider.dart';
+import 'package:optigo/utils/currency_formatter.dart';
+import 'package:optigo/widgets/build_widget.dart';
+import 'package:provider/provider.dart';
+
+class CombineTripsCard extends StatelessWidget {
+  final BookingModel booking;
+  const CombineTripsCard({super.key, required this.booking});
+
+  @override
+  Widget build(BuildContext context) {
+    final currencyFormat = NumberFormat.currency(locale: 'vi_VN', symbol: 'VNĐ');
+    return Container(
+      padding: EdgeInsets.all(16.sp),
+      margin: EdgeInsets.all(10.sp),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(booking.passengerName ?? "", style: GoogleFonts.lexend(fontSize: 16.sp, fontWeight: FontWeight.bold)),
+                  Text(Formatter.phoneFormatter(booking.passengerPhone ?? ""), style: GoogleFonts.lexend(fontSize: 14.sp, fontWeight: FontWeight.normal)),
+                ]
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(currencyFormat.format(booking.totalFare), style: GoogleFonts.lexend(fontSize: 16.sp, fontWeight: FontWeight.bold)),
+                  Text('${booking.paymentMethod}', style: GoogleFonts.lexend(fontSize: 14.sp, fontWeight: FontWeight.normal)),
+                ]
+              )
+            ]
+          ),
+          SizedBox(height: 10.h),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                children: [
+                  Icon(
+                    Icons.my_location,
+                    color: const Color(0xff176bac),
+                    size: 20.sp,
+                  ),
+                  Container(
+                    width: 1.w,
+                    height: MediaQuery.of(context).size.width * 0.15,
+                    color: Colors.grey[300],
+                  ),
+                  Icon(Icons.location_on, color: Colors.red, size: 20.sp),
+                ],
+              ),
+              SizedBox(width: 12.w),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BuildWidget.buildLocation(
+                      fullAddress: booking.pickupLocation! ,
+                    ),
+                    SizedBox(height: 25.h),
+                    BuildWidget.buildLocation(
+                      fullAddress: booking.dropOffLocation!,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 12.h),
+            child: Divider(height: 1, color: Colors.grey[400]),
+          ),
+          IntrinsicHeight(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('Quãng đường', style: GoogleFonts.lexend(fontSize: 14.sp, fontWeight: FontWeight.bold)),
+                      Text('${booking.distance?.toStringAsFixed(2)} km', style: GoogleFonts.lexend(fontSize: 14.sp, fontWeight: FontWeight.normal)),
+                    ],
+                  ),
+                  VerticalDivider(thickness: 2, color: Colors.grey[400],),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('Thời gian dự kiến', style: GoogleFonts.lexend(fontSize: 14.sp, fontWeight: FontWeight.bold)),
+                      Text('${booking.duration}', style: GoogleFonts.lexend(fontSize: 14.sp, fontWeight: FontWeight.normal)),
+                    ],
+                  )
+                ]
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.w, vertical:12.h),
+            child: Divider(height: 1, color: Colors.grey[400]),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.sp),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  child: ElevatedButton(
+                    onPressed: () {} ,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: const Color(0xff176bac),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.r),
+                        side: BorderSide(width: 2.w, color: const Color(0xff176bac)),
+                      ),
+                    ),
+                    child: Text(
+                      'Từ chối',
+                      style: GoogleFonts.lexend(
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8.w),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xfffedd59),
+                      foregroundColor: const Color(0xff176bac),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.r),
+                      ),
+                    ),
+                    child: Text(
+                      'Chấp nhận',
+                      style: GoogleFonts.lexend(fontSize: 16.sp,fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ]
+      ),
+    );
+  }
+}
